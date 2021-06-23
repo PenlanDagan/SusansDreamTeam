660a999421835f1897cae255a7dde5140e685632
tranqnhan <nt305748@gmail.com>
2021-06-23 17:33:13 -0500

    CI and S Models

---
+++ StudentDataApp/Models/Scholarship.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentDataApp.Models
+{
+    public class Scholarship
+    {
+        [Required(ErrorMessage = "Student ID is required")]
+        [Display(Name = "Student ID")]
+        [MaxLength(8)]
+        [Key]
+        public int StudentId { get; set; }
+
+        [Required(ErrorMessage = "Scholarship amount is required")]
+        [Display(Name = "Scholarship Amount")]
+        [DataType(DataType.Currency)]
+        public double ScholarshipAmount { get; set; }
+
+        [Display(Name = "Date Offered")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime DateOffered { get; set; }
+
+        [Display(Name = "Date Awarded")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime DateAwarded { get; set; }
+
+        [Display(Name = "Date Accepted")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime DateAccepted { get; set; }
+    }
+}
---
+++ StudentDataApp/Pages/ContactInfoPage/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using StudentDataApp.Data;
+using StudentDataApp.Models;
+
+namespace StudentDataApp.Pages.ContactInfoPage
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly StudentDataApp.Data.StudentDataAppContext _context;
+
+        public DetailsModel(StudentDataApp.Data.StudentDataAppContext context)
+        {
+            _context = context;
+        }
+
+        public ContactInfo ContactInfo { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ContactInfo = await _context.ContactInfo.FirstOrDefaultAsync(m => m.StudentId == id);
+
+            if (ContactInfo == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
