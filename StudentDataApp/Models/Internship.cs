615ffeb3e3e1166a51365155ea228c88d3fca4cb
hpeden96 <73446413+hpeden96@users.noreply.github.com>
2021-07-12 12:44:06 -0500

    Employment and intern added

---
+++ StudentDataApp/Models/Internship.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentDataApp.Models
+{
+    public class Internship
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public int StudentID { get; set; }
+        public Student Student { get; set; }
+
+        [Display(Name = "Internship Company Name")]
+        public string IntCompName { get; set; }
+
+        [Display(Name = "Internship Location")]
+        public string IntCompLocation { get; set; }
+
+        [Display(Name = "Internship Wage")]
+        [DataType(DataType.Currency)]
+        public double IntWage { get; set; }
+
+
+    }
+}
---
+++ StudentDataApp/Pages/InternshipPage/Create.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using StudentDataApp.Data;
+using StudentDataApp.Models;
+
+namespace StudentDataApp.Pages.InternshipPage
+{
+    public class CreateModel : PageModel
+    {
+        private readonly StudentDataApp.Data.StudentDataAppContext _context;
+
+        public CreateModel(StudentDataApp.Data.StudentDataAppContext context)
+        {
+            _context = context;
+        }
+
+        public int StudentID { get; set; }
+        public Student Student { get; set; }
+
+        public IActionResult OnGet(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            StudentID = (int)id;
+
+            return Page();
+        }
+
+        [BindProperty]
+        public Internship Internship { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Internship.Add(Internship);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Details", new { id = Internship.StudentID });
+        }
+    }
+}
