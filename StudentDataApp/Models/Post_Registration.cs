da9959c12a581d31678d89ae1b380409ea068b18
PenlanDagan <penlandagan@gmail.com>
2021-08-17 14:48:42 -0500

    redo

--- StudentDataApp/Models/Post_Registration.cs
+++ StudentDataApp/Models/Post_Registration.cs
@@ -21,20 +21,8 @@ namespace StudentDataApp.Models
         [Display(Name = "Schedule Status")]
         public bool schedComp { get; set; }
 
-
-        [Required(ErrorMessage = "missing start date")]
-        [Display(Name = "Start Date")]
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true, NullDisplayText = "Not Entered")]
-
-        public DateTime startDate { get; set; }
-
-
-        [Required(ErrorMessage = "require projected graduation date")]
-        [Display(Name = "Graduation Date")]
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true, NullDisplayText = "Not Entered")]
-        public DateTime gradDate { get; set; }
+        [DataType(DataType.Text)]
+        public string StartTerm { get; set; }
 
 
         [Required(ErrorMessage = "require emphasis")]
