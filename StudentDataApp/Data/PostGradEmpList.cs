c29d33264e84dd1765e53cbd74b0ba4f05ea75a7
hpeden96 <73446413+hpeden96@users.noreply.github.com>
2021-08-17 23:46:00 -0500

    LName and Emp

---
+++ StudentDataApp/Data/PostGradEmpList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace StudentDataApp.Data
+{
+    public class PostGradEmpList
+    {
+        private static List<SelectListItem> empSelectList;
+
+        public static List<SelectListItem> getItems()
+        {
+
+            empSelectList = new List<SelectListItem>();
+
+            empSelectList.Add(new SelectListItem { Value = "Mainframe", Text = "Mainframe" });
+            empSelectList.Add(new SelectListItem { Value = "Web Dev", Text = "Web Dev" });
+            empSelectList.Add(new SelectListItem { Value = "Mobile", Text = "Mobile" });
+            
+
+            return empSelectList;
+        }
+    }
+}
